3cc2297a4de839e9db4478587af1e376cb1db77d
bxjbxj8899 <98626155+bxjbxj8899@users.noreply.github.com>
2025-09-30 07:55:39 +0800

    Delete QX/jd_jingfen.js

--- QX/jd_jingfen.js
+++
@@ -1,6 +0,0 @@
-[mitm]
-hostname = *.jd.com
-
-[rewrite_local]
-^https://item\.jd\.com/.*\.html url script-response-body https://raw.githubusercontent.com/username/repo/main/jd_jingfen.js
-^https://union\.jd\.com/api/receivecode/getCode url script-response-body https://raw.githubusercontent.com/username/repo/main/jd_jingfen.js
